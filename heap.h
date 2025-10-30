727106a3c6a24f662bffce4ae65e896e80ad1197
Pranith <pbollampally4617@sdsu.edu>
2025-10-29 22:10:09 -0700

    Implementing array min-heap

--- heap.h
+++ heap.h
@@ -5,9 +5,8 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-#include <iostream>
-using namespace std;
-
+// Simple array min-heap that stores NODE INDICES.
+// Comparisons are done using the external weightArr[] passed in.
 struct MinHeap {
     int data[64];
     int size;
@@ -15,21 +14,43 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+        // insert at end then bubble up
+        data[size] = idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        // remove root, move last to root, then bubble down
+        int ret = data[0];
+        size--;
+        data[0] = data[size];
+        downheap(0, weightArr);
+        return ret;
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+        while (pos > 0) {
+            int p = (pos - 1) / 2;
+            if (weightArr[data[pos]] < weightArr[data[p]]) {
+                int tmp = data[pos]; data[pos] = data[p]; data[p] = tmp;
+                pos = p;
+            } else break;
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        while (true) {
+            int l = 2 * pos + 1;
+            int r = 2 * pos + 2;
+            int s = pos;
+            if (l < size && weightArr[data[l]] < weightArr[data[s]]) s = l;
+            if (r < size && weightArr[data[r]] < weightArr[data[s]]) s = r;
+            if (s != pos) {
+                int tmp = data[pos]; data[pos] = data[s]; data[s] = tmp;
+                pos = s;
+            } else break;
+        }
     }
 };
 
